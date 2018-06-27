04f75a234b59d462d0cd32ae8e9fe82d4fec41c8
Rafostar <raf.dziegiel@gmail.com>
2018-06-27 14:21:03 +0200

    Started implementing "open file and stream" function

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <string>
+#include <sstream>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +18,14 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_streamButton_clicked()
+{
+    string CastFile="This File";
+    stringstream ss;
+    ss << "echo " << CastFile;
+    string tmp = ss.str();
+    const char* CastCommand = tmp.c_str();
+    system(CastCommand);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_streamButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
